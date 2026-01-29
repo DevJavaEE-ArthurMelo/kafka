946d4238e3ab25093eb77cbf305172f770fd47c8
DevJavaEE-ArthurMelo <arthur.etps@gmail.com>
2026-01-29 13:37:25 -0300

    Alterações no envio de mensagens

---
+++ src/main/java/com/arthur/strproducer/config/KafkaProperties.java
@@ -0,0 +1,21 @@
+package com.arthur.strproducer.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.kafka")
+public class KafkaProperties {
+    
+    private String bootstrapServers;
+    
+    public String getBootstrapServers() {
+        return bootstrapServers;
+    }
+    
+    public void setBootstrapServers(String bootstrapServers) {
+        this.bootstrapServers = bootstrapServers;
+    }
+}
